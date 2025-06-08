23016791ac6dde3247d71a4c915f199728ddac3f
Xiaoyi <128590175+MiziClare@users.noreply.github.com>
2025-06-09 00:45:38 +0100

    Add files via upload

---
+++ Assignment1 for Data Cleaning/Data_Processing.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+function load_JSON(file_name){
+    let output_file = undefined;
+    // Todo: Implement this function
+    return output_file;
+}
+
+function clean_name(entries){
+    // Todo: Implement this function
+    return entries;
+}
+
+function clean_price(entries, product_prices){
+    // Todo: Implement this function
+    return entries;
+}
+
+function clean_category(entries){
+    // Todo: Implement this function
+    return entries;
+}
+
+function clean_type(entries){
+    // Todo: Implement this function
+    return entries;
+}
+
+function clean_quantity(entries){
+    // Todo: Implement this function
+    return entries;
+}
+
+function clean_rating(entries){
+    // Todo: Implement this function
+    return entries;
+}
+
+function clean_image_link(entries){
+    // Todo: Implement this function
+    return entries;
+}
+
+module.exports = {load_JSON, clean_name, clean_price, clean_category, clean_type, clean_quantity, clean_rating, clean_image_link};
